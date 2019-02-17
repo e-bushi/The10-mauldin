b4a900204cd75f4e05ccc1dec00c679e784a10a8
e-bushi <chrislovemauldin@gmail.com>
2019-02-17 01:08:59 -0500

    Added Launch Screen

---
+++ The10-Mauldin/LaunchScreenViewController.swift
@@ -0,0 +1,50 @@
+//
+//  LaunchScreenViewController.swift
+//  The10-Mauldin
+//
+//  Created by Chris Mauldin on 2/17/19.
+//  Copyright © 2019 Chris Mauldin. All rights reserved.
+//
+
+import UIKit
+
+class LaunchScreenViewController: UIViewController {
+    
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var projectNameLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            self.animateLogo()
+        }
+    }
+    
+    func animateLogo() {
+        logo.transform = .init(scaleX: 1.5, y: 1.5)
+        UIView.animate(withDuration: 1.0, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 5.0, options: [.autoreverse, .curveEaseIn], animations: {
+            self.logo.transform = .identity
+
+        }) { (_) in
+            UIView.animate(withDuration: 0.3, animations: {
+                self.projectNameLabel.transform = .init(translationX: 400.0, y: 0)
+            }, completion: { (_) in
+                self.performSegue(withIdentifier: "showApp", sender: self)
+            })
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
